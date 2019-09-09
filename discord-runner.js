3f95534710bf293ef7a54a6d2c06cbaf5ac4f5c6
Drew Facchiano <andrewfacchino@gmail.com>
2019-09-09 14:54:34 -0700

    Poll revert

--- discord-runner.js
+++ discord-runner.js
@@ -15,12 +15,13 @@ client.on('message', msg => {
 		msg.reply(messages.motivation_tips[Math.floor(Math.random() * messages.motivation_tips.length)]);
 	} else if(messages.known_scum.some(word => msg.content.includes(word))){
 		msg.reply("Scum!")
-	} else if (messages.poll_commands.includes(msg.content)){
-		const params = msg.content.split(" ");
-		console.log(params)
-		//https://www.npmjs.com/package/discord.js-poll-embed
-		pollEmbed(msg, "title", "1,2,3,4,5", 30, emojiList);
 	}
+	// else if (messages.poll_commands.includes(msg.content)){
+	// 	const params = msg.content.split(" ");
+	// 	console.log(params)
+	// 	//https://www.npmjs.com/package/discord.js-poll-embed
+	// 	pollEmbed(msg, "title", "1,2,3,4,5", 30, emojiList);
+	// }
 });
 
 client.login(process.env.BOT_TOKEN);
